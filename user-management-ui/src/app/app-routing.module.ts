ad490be4b1c77c357f74b704815031d06284ecda
Ashkan Zarifian <zs.ashkan@yahoo.com>
2019-03-14 18:12:29 +0330

    add app routing

--- user-management-ui/src/app/app-routing.module.ts
+++ user-management-ui/src/app/app-routing.module.ts
@@ -1,10 +1,20 @@
-import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
+import {NgModule} from '@angular/core';
+import {RouterModule, Routes} from '@angular/router';
+import {UserComponent} from './user/user.component';
+import {AddUserComponent} from './user/add-user/add-user.component';
+import {EditUserComponent} from './user/edit-user/edit-user.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', component: UserComponent},
+  {path: 'home', component: UserComponent},
+  {path: 'addUser', component: AddUserComponent},
+  {path: 'user/:id/edit', component: EditUserComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {
+
+}
